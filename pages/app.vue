1f514fb9da9f5e4e835fb9ffe4d529dcc5641a7a
ACJT123 <acjtacjt@gmail.com>
2024-09-04 23:43:06 +0800

    break(content): move the content folder

---
+++ pages/app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const msg = ref("Hello Vue 3 + Vite + TypeScript");
+</script>
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">ACJT Daily Progress Blog</NuxtLink></li>
+        <li><NuxtLink to="/posts/1">Post 1</NuxtLink></li>
+        <li><NuxtLink to="/posts/2">Post 2</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <Header></Header>
+  <ContentDoc/>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import Header from "./components/header.vue";
+</script>
+
+<style scoped></style>
