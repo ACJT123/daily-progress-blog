cc594b84d16c5f1bad23bfd059770077e00852f5
ACJT123 <acjtacjt@gmail.com>
2024-09-04 23:03:48 +0800

    feat(package): install tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const msg = ref("Hello Vue 3 + Vite + TypeScript");
+</script>
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="bg-red-400">Vue 3 + Vite + TypeScript</h1>
+    <p>{{ msg }}</p>
   </div>
 </template>
