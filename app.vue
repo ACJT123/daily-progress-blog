0a4c0962059a55ea477e47e3411a3cf1474f4f98
ACJT123 <acjtacjt@gmail.com>
2024-09-09 14:54:04 +0800

    feat: add gtm

--- app.vue
+++ app.vue
@@ -1,4 +1,20 @@
 <template>
+  <Head>
+    <!-- Google tag (gtag.js) -->
+    <Script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-DXK6FG1JX7"
+    ></Script>
+    <Script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+
+      gtag("config", "G-DXK6FG1JX7");
+    </Script>
+  </Head>
   <Header />
 
   <Layout>
