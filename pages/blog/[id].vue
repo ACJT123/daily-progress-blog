25bbec01d232cb10c135327990146d94662cf913
ACJT123 <acjtacjt@gmail.com>
2024-09-13 15:09:05 +0800

    break(blog): create table of content component

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -1,15 +1,12 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 import { useRoute } from "nuxt/app";
-import {
-  BlockType,
-  type SingleBlog,
-  type SingleBlogResponse,
-} from "~/server/types/blog";
+import { type SingleBlog, type SingleBlogResponse } from "~/server/types/blog";
 
 import { DateTime } from "luxon";
 import Content from "~/components/blog/content.vue";
 import Skeleton from "~/components/skeleton.vue";
+import tableOfContent from "~/components/blog/table-of-content.vue";
 import { SkeletonType } from "~/server/types/skeleton";
 
 const blog = ref<SingleBlog>({
@@ -46,28 +43,7 @@ fetchBlog();
   </template>
 
   <div v-else class="flex flex-col md:flex-row gap-10 pb-4 pt-6 relative">
-    <aside
-      class="bg-green-400/20 rounded-md px-4 py-2 pb-4 w-[200px] h-fit sticky top-4 hidden md:block"
-    >
-      <h1 class="border-b-2 border-white/10 p-2 pt-0 pl-0">Table Of Content</h1>
-
-      <p v-for="block in blog.block" class="text-[12px]">
-        <template
-          v-if="
-            block.type === BlockType.HEADING_1 ||
-            block.type === BlockType.HEADING_2 ||
-            block.type === BlockType.HEADING_3
-          "
-        >
-          <a
-            :href="'#' + block[block.type]?.rich_text[0]?.text?.content"
-            class="block mt-2 opacity-70 hover:opacity-100 hover:underline"
-          >
-            {{ block[block.type]?.rich_text[0]?.text?.content }}
-          </a>
-        </template>
-      </p>
-    </aside>
+    <tableOfContent :blog="blog" />
 
     <div class="flex-1">
       <h1 class="font-bold text-2xl text-center">
